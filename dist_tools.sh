2f407be8ffc21fa0688d4108c66a126fa902bd3a
Champion Red <championred69@gmail.com>
2024-05-13 17:49:57 -0600

    Changed offset generation

--- dist_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-mkdir dist
-grep -f symbols.txt pokefirered_rev1.map > dist/offsets.txt
-tools/inigen/inigen pokefirered_rev1.elf dist/custom_offsets.ini --code BPRE --name "FireRed Moveset Expansion"
