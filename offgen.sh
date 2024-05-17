d30a5fd440343be2a42f04d1db0bab8cd5787049
Champion Red <championred69@gmail.com>
2024-05-17 15:56:22 -0600

    Add offsets to JAR

--- offgen.sh
+++ offgen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HACK="Moveset Expansion"
+CONFIG=com/dabomstew/pkrandom/config
 
 case `pwd` in
 *firered)
@@ -18,7 +19,8 @@ HACK="Emerald $HACK"
 esac
 
 rm -rf dist
-mkdir dist
+mkdir -p dist/$CONFIG
 grep -f symbols.txt $TARGET.map | tr -s ' ' | cut --delimiter=' ' -f2-3 > dist/offsets.txt
-tools/inigen/inigen $TARGET.elf dist/custom_offsets.ini --code $CODE --name "$HACK"
+tools/inigen/inigen $TARGET.elf dist/$CONFIG/custom_offsets.ini --code $CODE --name "$HACK"
+jar uf upr.jar -C dist $CONFIG/custom_offsets.ini
 cat dist/offsets.txt | node -r fs -p "JSON.stringify(fs.readFileSync(0,'utf8').split(/\s+/).slice(0,-1).reduce((acc,val,ind,arr)=>acc[val]?acc:Object.assign(acc,{[arr[ind+1]]:Number(val)}),{}));" > dist/offsets$SUFFIX.json
